00a0ef22014687261b979a6379489772b1e00e80
Chris Ayoub <chris.ayoubtexas@gmail.com>
2020-04-13 18:39:56 -0500

    Trying some graph library, add input for init state index

--- scripts.js
+++ scripts.js
@@ -7,6 +7,9 @@ window.onload = () => {
     // Set update button functionality
     document.getElementById("update").onclick = doUpdate;
 
+    // TODO DELETE THIS, here for testing
+    drawGraph()
+
     // Set default text of text area
     getInputElem().value =
 `# Commented-lines here are expressed with '#'
@@ -38,12 +41,19 @@ function doUpdate() {
     // MARK -- Parse and validate input
 
     const tapeValue = document.getElementById("tape").value;
+    const stateIndex = document.getElementById("stateIndex").value;
     const ruleText = getInputElem().value;
 
     // Validate tape input
     const tapeRegex = RegExp("^[01]+$");
     if (!tapeRegex.test(tapeValue)) {
         alert("Error: tape is empty or contains invalid characters.")
+        return;
+    }
+
+    // Validate state index
+    if (!stateIndex || isNaN(stateIndex) || stateIndex < 0 || stateIndex >= tapeValue.length) {
+        alert("Error: initial state index should be in-bounds of the tape, and a number.")
         return;
     }
 
@@ -64,151 +74,15 @@ function doUpdate() {
 
     // MARK -- Formulate output / display
 
-    // TODO complete, need to draw elements on the Canvas with possible forward/backward states
+    // TODO complete, need to draw elements on the graph with possible forward/backward states
     // Can adhere to some limit
 
-    // TODO alter this call, simply here for testing
-    configCanvas();
 }
 
-function drawTmState(tape, state, stateIndex, layer, x, y) {
-    const tapeVal = tape.join('') + "\n" + ' '.repeat(stateIndex) + state
-
-    const tapeText = new Konva.Text({
-        x: x,
-        y: y,
-        text: tapeVal,
-        fontSize: 20,
-        fontFamily:  'Courier',
-        fontStyle: 'bold',
-        padding: 16
-    });
-    tapeText.offsetX(tapeText.width() / 2);
-    tapeText.offsetY(tapeText.height() / 2);
-
-    const box = new Konva.Rect({
-        x: x,
-        y: y,
-        stroke: '#555',
-        strokeWidth: 1,
-        fill: '#ddd',
-        width: tapeText.width(),
-        height: tapeText.height(),
-        cornerRadius: 20
-    });
-    box.offsetX(box.width() / 2);
-    box.offsetY(box.height() / 2);
-
-    layer.add(box);
-    layer.add(tapeText);
-
-    return box;
-}
-
-function drawLine(x1, y1, x2, y2, layer) {
-    const line = new Konva.Arrow({
-        points: [x1, y1, x2, y2],
-        pointerLength: 10,
-        pointerWidth: 10,
-        fill: 'black',
-        stroke: 'black',
-        strokeWidth: 2
-    });
-    layer.add(line);
-}
-
-function allowStageZoom(stage) {
-    // https://konvajs.org/docs/sandbox/Zooming_Relative_To_Pointer.html
-    const scaleBy = 1.1;
-    stage.on('wheel', e => {
-        e.evt.preventDefault();
-        const oldScale = stage.scaleX();
-
-        const mousePointTo = {
-            x: stage.getPointerPosition().x / oldScale - stage.x() / oldScale,
-            y: stage.getPointerPosition().y / oldScale - stage.y() / oldScale
-        };
-
-        const newScale =
-            e.evt.deltaY < 0 ? oldScale * scaleBy : oldScale / scaleBy;
-        stage.scale({ x: newScale, y: newScale });
-
-        const newPos = {
-            x:
-                -(mousePointTo.x - stage.getPointerPosition().x / newScale) *
-                newScale,
-            y:
-                -(mousePointTo.y - stage.getPointerPosition().y / newScale) *
-                newScale
-        };
-        stage.position(newPos);
-        stage.batchDraw();
-    });
-}
-
-// Draws a central box, with immediate previous and next transitions
-function drawBoxWithTransitions(prev, curr, fwd, originX, originY, layer) {
-    const currBox = drawTmState(curr.tape, curr.state, curr.stateIndex, layer, originX, originY);
-    // Can set color of our 'current' TM state, if desired
-    currBox.fill('#85b942');
-
-    // Helper values
-    const X_GAP = 20;
-    const Y_GAP = 10;
-    const colSep = (currBox.width() * 1.5) + X_GAP;
-    const getInitY = (arrLength) => currBox.y() - currBox.height() / 2 + (arrLength / 2 * (Y_GAP + currBox.height()));
-    const yChange = Y_GAP + currBox.height();
-
-    const drawLineHelper = (startBox, endBox) => {
-        const x1 = startBox.x() + (startBox.width() / 2);
-        const y1 = startBox.y();
-        const x2 = endBox.x() - (endBox.width() / 2);
-        const y2 = endBox.y();
-        drawLine(x1, y1, x2, y2, layer);
-    }
-
-    // Previous boxes
-    const prevX = currBox.x() - colSep;
-    let prevY = getInitY(prev.length);
-    for (let c of prev) {
-        const newBox = drawTmState(c.tape, c.state, c.stateIndex, layer, prevX, prevY);
-        drawLineHelper(newBox, currBox);
-        prevY -= yChange;
-    }
-
-    // Next boxes
-    const nextX = currBox.x() + colSep;
-    let nextY = getInitY(fwd.length);
-    for (let c of fwd) {
-        const newBox = drawTmState(c.tape, c.state, c.stateIndex, layer, nextX, nextY);
-        drawLineHelper(currBox, newBox);
-        nextY -= yChange;
-    }
-}
-
-// TODO update this
-// https://konvajs.org/docs/overview.html
-function configCanvas(rules, tape) {
-    const elem = document.getElementById('content');
-    const height = elem.clientHeight;
-    const width = elem.clientWidth;
-
-    const stage = new Konva.Stage({
-        container: 'content',
-        width: width,
-        height: height,
-        draggable: true
-    });
-    allowStageZoom(stage);
-    const layer = new Konva.Layer();
-
-    // Example instances, note that the transitions do not make sense
+function drawGraph() {
+    // Sample DATA - Example instances, note that the transitions do not make sense
     const sampleTm = '0011010'.split('');
-    // TODO: Actually use the given input rules to figure out how to continually generate previous/forwqrd states from the current
-    // Could use recursion?
-    // Note that the given code might need to be fixed for displaying. Say "k" in next is discovered to have a unique previous.
-    // Currently, will probably write on top of "d" our "curr". Will need to eventually fix this problem
-    let prev = [
+    const prev = [
         {
             'tape': sampleTm,
             'state': 'a',
@@ -244,8 +118,98 @@ function configCanvas(rules, tape) {
         }
     ]
 
-    // Draws the above sample boxes
-    drawBoxWithTransitions(prev, curr, fwd, width / 2, height / 2, layer);
+    const getNodeLbl = (tm) => tm.tape.join('') + "\n" + ' '.repeat(tm.stateIndex) + tm.state
+    const elem = document.getElementById('content');
+    const height = elem.clientHeight;
+    const width = elem.clientWidth;
 
-    stage.add(layer);
+    let id = 0;
+    const cy = cytoscape({
+        container: document.getElementById('content'),
+        // autolock: true,
+        minZoom: 0.1,
+        maxZoom: 2,
+        style: [
+            {
+                selector: 'node',
+                style: {
+                    label: 'data(lbl)',
+                    'font-family': 'Courier',
+                    'font-size': 20,
+                    'font-style': 'bold',
+                    padding: 12,
+                    'background-color': '#d1d1d1',
+                    'border-width': '1',
+                    'border-color': 'black',
+                    width: 'label',
+                    height: 'label',
+                    shape: 'rectangle',
+                    'text-justification': 'center',
+                    'text-valign': 'center',
+                    'text-wrap': 'wrap'
+                }
+            },
+            {
+                selector: 'edge',
+                style: {
+                    'curve-style': 'bezier',
+                    'target-arrow-shape': 'triangle',
+                    'width': 5,
+                }
+            }
+        ]
+    });
+
+    const currId = id;
+    cy.add({
+        data: {
+            id: id++,
+            lbl: getNodeLbl(curr)
+        },
+        position: {
+            x: width / 2,
+            y: height / 2,
+        }
+    })
+
+    for (let p of prev) {
+        const thisId = id++;
+        cy.add({
+            data: {
+                id: thisId,
+                lbl: getNodeLbl(p),
+            }
+        })
+        cy.add({
+            data: {
+                id: id++,
+                source: thisId,
+                target: currId,
+            }
+        })
+    }
+
+    for (let n of fwd) {
+        const thisId = id++;
+        cy.add({
+            data: {
+                id: thisId,
+                lbl: getNodeLbl(n),
+            }
+        })
+        cy.add({
+            data: {
+                id: id++,
+                source: currId,
+                target: thisId
+            }
+        })
+    }
+
+    cy.layout({
+        name: 'breadthfirst',
+        directed: true,
+        grid: true,
+        spacingFactor: 1.1,
+    }).run();
 }
