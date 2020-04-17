e6ca0e0deda6f331c2c19e834a887b9d14dbd39b
chris <chris.ayoubtexas@gmail.com>
2020-04-17 04:05:28 -0500

    Different rule validation, recursive state generation

--- scripts.js
+++ scripts.js
@@ -7,9 +7,6 @@ window.onload = () => {
     // Set update button functionality
     document.getElementById("update").onclick = doUpdate;
 
-    // TODO DELETE THIS, here for testing
-    //drawGraph()
-
     // Set default text of text area
     getInputElem().value =
 `# Commented-lines here are expressed with '#'
@@ -18,6 +15,7 @@ window.onload = () => {
 # a,1
 # 0,b
 # This means that if in state 'a' and we read a '1', write a '0' and move to state 'b'.
+# Use a dash '-' to indicate a blank.
 
 # Syntax for movement rules:
 # b
@@ -36,6 +34,201 @@ b
 `;
 };
 
+function parseRules(lines) {
+    if (lines.length % 2 !== 0){
+        alert("Error: incomplete rule pair detected.");
+        return false;
+    }
+
+    const letterRegex = RegExp("[a-z]");
+    const numRegex = RegExp("[01-]");
+    const directionRegex = RegExp("[<>]");
+
+    let readWriteMap = new Map();
+    let movementMap = new Map();
+
+    for (let i = 0; i < lines.length - 1; i += 2) {
+        const firstLine = lines[i];
+        const secondLine = lines[i + 1];
+
+        const firstToks = firstLine.split(',');
+        const secondToks = secondLine.split(',');
+
+        const firstLen = firstToks.length;
+        const secondLen = secondToks.length;
+
+        if (firstLen <= 0 || firstLen > 2) {
+            alert("Error: cannot have more than 2 tokens on a line.");
+            return false;
+        } else if (secondLen !== 2) {
+            alert("Error: second line in a rule must have 2 tokens.");
+            return false;
+        }
+
+        if (firstLen === 1) {
+            // Movement rule
+            const startSymbol = firstToks[0];
+            const destSymbol = secondToks[1];
+            const move = secondToks[0];
+            if (!letterRegex.test(startSymbol) || !letterRegex.test(destSymbol) || !directionRegex.test(move)) {
+                alert("Error: invalid input in movement rules");
+                return false;
+            }
+
+            // Map the key to an array of possible transitions
+            if (!movementMap.get(firstLine)) {
+                movementMap.set(firstLine, []);
+            }
+            movementMap.get(firstLine).push([move, destSymbol]);
+        } else {
+            // R/W rule
+            const startSymbol = firstToks[0];
+            const readBit = firstToks[0];
+            const writeBit = secondToks[0];
+            const destSymbol = secondToks[1];
+
+            if (!letterRegex.test(startSymbol) || !letterRegex.test(destSymbol) ||
+                !numRegex.test(readBit) || !numRegex.test(writeBit)) {
+                alert("Error: invalid input in read/write rules");
+                return false;
+            }
+
+            // Map the key to an array of possible transitions
+            if (!readWriteMap.get(firstLine)) {
+                readWriteMap.set(firstLine, []);
+            }
+            readWriteMap.get(firstLine).push([writeBit, destSymbol]);
+        }
+    }
+
+    return {
+        readWriteMap,
+        movementMap
+    }
+}
+
+let globalId = 0;
+const TAPE_BLANK = '-';
+
+// Return a list of possible states from the current state
+function resolveStates(currentState, currentDepth, readWriteRules, movementRules) {
+    const resultNodes = [];
+    const resultEdges = [];
+
+    // Limit on our depth here
+    if (currentDepth >= 5) {
+        return {
+            resultNodes,
+            resultEdges
+        };
+    }
+
+    // Adorn extra properties needed
+    const id = globalId++; // Use global counter for unique IDs
+    currentState.id = id;
+    currentState.depth = currentDepth;
+    // Copy of object in result
+    resultNodes.push(...currentState);
+
+    // Backup our initial values, to restore later
+    const initState = currentState.state;
+    const initIndex = currentState.stateIndex;
+    const initTape = [...currentState.tape];
+    const initBit = initTape[initIndex];
+
+    const transforms = {
+        readWriteRules: {
+            key: initState + ',' + initBit, // Defines key into readWriteRules map
+            // Function on how to apply a R/W rule
+            fn: (toChange, rule) => {
+                const writeBit = rule[0];
+                const destState = rule[1];
+
+                toChange.tape[initIndex] = writeBit;
+                toChange.state = destState;
+            }
+        },
+        movementRules: {
+            key: initState, // Defines key into movementRules map
+            // Function on how to apply a movement rule
+            fn: (toChange, rule) => {
+                const move = rule[0];
+                const destState = rule[1];
+
+                toChange.state = destState;
+                if (move === '<') {
+                    // Move left
+                    if (initIndex === 0) {
+                        // Add blank to front if at left end of tape
+                        toChange.tape.unshift(TAPE_BLANK);
+                    } else {
+                        // Adjust index only if did not add to front of list
+                        toChange.stateIndex = initIndex - 1;
+                    }
+                } else if (move === '>') {
+                    // Move right
+                    if (initIndex === toChange.tape.length - 1) {
+                        // Add blank to end if at right end of tape
+                        toChange.tape.push(TAPE_BLANK);
+                    }
+                    // Always adjust index
+                    toChange.stateIndex = initIndex + 1;
+                }
+            }
+        }
+    };
+
+    // For all kinds of transformation rules...
+    for (let ruleSet of transforms.keys()) {
+        // Grab the transform key and relevant function
+        const key = transforms[ruleSet].key;
+        const transform = transforms[ruleSet].fn;
+
+        // Get rules from the set, based on the defined key
+        let rules = ruleSet.get(key);
+        if (!rules) {
+            // If no rules resolve, just make empty list (do nothing below)
+            rules = [];
+        }
+
+        // For all rules that apply...
+        for (let rule of rules) {
+            // Apply the transition given
+            transform(currentState, rule);
+
+            // Try to recurse on this
+            const newDepth = currentDepth + 1;
+            const subResult = resolveStates(currentState, newDepth, readWriteRules, movementRules);
+
+            // Calculate any edges
+            const newEdges = subResult.resultNodes.filter(n => n.depth === newDepth)
+                .map(n => {
+                    return {
+                        id: globalId++,
+                        source: id,
+                        target: n.id
+                    }
+                });
+
+            // Add to total result
+            resultNodes.push.apply(subResult.resultNodes);
+            resultEdges.push.apply(subResult.resultEdges);
+            resultEdges.push.apply(newEdges);
+
+            // Restore the state completely for next transformation in the loop
+            currentState.tape = [...initTape];
+            currentState.state = initState;
+            currentState.stateIndex = initIndex;
+        }
+    }
+
+    // Return final result
+    return {
+        resultNodes,
+        resultEdges
+    };
+}
+
 // Function that grabs input and updates the display to reflect the input
 function doUpdate() {
     // MARK -- Parse and validate input
@@ -46,16 +239,14 @@ function doUpdate() {
 
     // Validate tape input
     const tapeRegex = RegExp("^[01]+$");
-
-
     if (!tapeRegex.test(tapeValue)) {
-        alert("Error: tape is empty or contains invalid characters.")
+        alert("Error: tape is empty or contains invalid characters.");
         return;
     }
 
     // Validate state index
     if (!stateIndex || isNaN(stateIndex) || stateIndex < 0 || stateIndex >= tapeValue.length) {
-        alert("Error: initial state index should be in-bounds of the tape, and a number.")
+        alert("Error: initial state index should be in-bounds of the tape, and a number.");
         return;
     }
 
@@ -65,190 +256,51 @@ function doUpdate() {
         .map(line => line.replace(/^\s+|\s+$/g, '')) // Trim strings
         .filter(line => !line.startsWith("#") && line.length > 0); // Remove comments and blank lines
 
-    // validate that the rules are valid
-    if(lines.length % 2 != 0){
-        alert("Error: incomplete rule detected");
-    }
-
-    const letterRegex = RegExp("[a-z]");
-    const numRegex = RegExp("[01]");
-    const directionRegex = RegExp("[<>]");
-    
-    movementCheck = false;
-    
-    for (let [lineNum, l] of lines.entries()){
-        if(l.length == 1 && letterRegex.test(l[0]) && lineNum % 2 == 0){
-            movementCheck = true;
-        }
-        if(l.length == 1 && letterRegex.test(l[0]) && lineNum %2 == 1){
-            alert("Error: incomplete read/write rule detected");
-        }
-
-        // Check read/write rules
-        if(!movementCheck){
-            if(lineNum % 2 == 0){
-                if(l.length != 3 || !letterRegex.test(l[0]) || l[1] != ',' || !numRegex.test(l[2])){
-                    alert("Error: invalid input in read/write rules");
-                }
-            }
-            else{
-                if(l.length != 3 || !numRegex.test(l[0]) || l[1] != ',' || !letterRegex.test(l[2])){
-                    alert("Error: invalid input in read/write rules");
-                }
-            }
-        }
-        // Check movement rules
-        else{
-            if(lineNum % 2 == 0){
-                if(l.length != 1 || !letterRegex.test(l[0])){
-                    alert("Error: invalid input in movement rules");
-                }
-            }
-            else{
-                if(l.length != 3 || !directionRegex.test(l[0]) || l[1] != ',' || !letterRegex.test(l[2])){
-                    alert("Error: invalid input in movement rules");
-                }
-            }
-        }
-    }
-
-
-    // Represent current tape with array
-    // Initial position is index 0, initial state is 'a'
-    // TODO rather than assuming this initial position, let's add something to accept this as input (?)
-    let tape = tapeValue.split("");
-
-    curr = {
-        'tape': tape,
-        'state': 'a',
-        'stateIndex': stateIndex
+    // Parse and validate that the rules are valid
+    const result = parseRules(lines);
+    if (result === false) {
+        return;
     }
 
     // Saving TM rules in read/write and movement maps
-    let readWriteMap = new Map()
-    let movementMap = new Map()
+    const readWriteMap = result.readWriteMap;
+    const movementMap = result.movementMap;
 
-    movementCheck = false;
+    // Represent current tape with array
+    // Initial position is index `stateIndex`, initial state is 'a'
+    let tape = tapeValue.split("");
 
-    for (let [lineNum, l] of lines.entries()){
-        if(lineNum % 2 != 0){
-            continue;
-        }
+    // Now, in a recursive fashion, let us figure out all possible "forward" states from the "current"
+    // We will impose a temporary limit on the "depth".
+    // We also need to note down a mapping of "edges".
+    const initState = {
+        tape,
+        'state': 'a',
+        stateIndex
+    };
 
-        if(l.length == 1 && letterRegex.test(l[0])){
-            movementCheck = true;
-        }
+    const forwardResult = resolveStates(initState, 0, readWriteMap, movementMap);
+    const forwardNodes = forwardResult.resultNodes;
+    const forwardEdges = forwardResult.resultEdges;
 
-        if(!movementCheck){
-            readWriteMap.set(l,lines[lineNum+1]);   // i.e. 'a,1' -> '0,b'
-        }
-        else{
-            movementMap.set(l,lines[lineNum+1]);    // i.e. 'b' -> '<,a'
-        }
-    }
+    // Now, we will reverse the rules in the maps and recursively get the backward states
+    // TODO implement this!
+    // TODO for the resulting edges, will need to flip them
+    // Or, set some flag in resolveStates
 
     // MARK -- Formulate output / display
-    currTape = tape;
-    currIndex = stateIndex;
-    currState = 'a';
-    prev = []
-    fwd = []
-
-    var workTapes = [curr];
-
-    while(workTapes.length > 0){
-        newTape = false;
-        newElem = {};
-
-        elem = workTapes.pop();
-        tape = elem.tape;
-        state = elem.state;
-        index = elem.stateIndex;
-
-        newElem.tape = String(tape);
-        newElem.state = String(state);
-        newElem.index = String(index);
-
-        if(rule = readWriteMap.get(String(state + ',' + tape[index]))){
-            newElem.tape[index] = rule[0];
-            newElem.state = rule[2];
-            newTape = true;
-        }
-
-        if(rule = movementMap.get(String(elem.state))){
-            if(rule[0] == '>'){
-                newElem.stateIndex = String(parseInt(index) + 1);
-            }
-            else {
-                newElem.stateIndex = String(parseInt(index) - 1);
-            }
-
-            // Think about padding '_' on either end later?
-            if(parseInt(elem.stateIndex >= 0)){
-                newTape = true;
-            }
-            else{
-                newTape = false;
-            }
-
-        }
-
-        if(newTape){
-            workTapes.push(newElem);
-            fwd.push(newElem);
-        }
-    }
-
-    // TODO complete, need to draw elements on the graph with possible forward/backward states
-    // Can adhere to some limit
-
-    drawGraph(curr);
+    forwardNodes.push(initState); // TODO remove this line, temporary
+    drawGraph(nodes, edges);
 }
 
-function drawGraph(curr) {
-    // Sample DATA - Example instances, note that the transitions do not make sense
-    const sampleTm = '0011010'.split('');
-    const prev = [
-        {
-            'tape': sampleTm,
-            'state': 'a',
-            'stateIndex': 0
-        },
-        {
-            'tape': sampleTm,
-            'state': 'b',
-            'stateIndex': 1
-        },
-        {
-            'tape': sampleTm,
-            'state': 'e',
-            'stateIndex': 6
-        }
-    ]
-
-    const fwd = [
-        {
-            'tape': sampleTm,
-            'state': 'j',
-            'stateIndex': 5
-        },
-        {
-            'tape': sampleTm,
-            'state': 'k',
-            'stateIndex': 4
-        }
-    ]
-
+function drawGraph(nodes, edges) {
     // Function to create the text on a node
-    const getNodeLbl = (tm, level) => {
+    const getNodeLbl = (tm) => {
         let result = tm.tape.join('') + "\n";
         result += ' '.repeat(tm.stateIndex) + tm.state + "\n";
-        result += "Level: " + level;
+        result += "Level: " + tm.depth;
         return result;
-    }
-
-    // Nodes and edges need unique IDs
-    let id = 0;
+    };
 
     // Setup the graph
     const cy = cytoscape({
@@ -288,49 +340,23 @@ function drawGraph(curr) {
         ]
     });
 
-    // Node for current
-    const currId = id;
-    const currLevel = 0;
-    cy.add({
-        data: {
-            id: id++,
-            lbl: getNodeLbl(curr, currLevel),
-        }
-    })
+    // Nodes and edges need unique IDs
+    let id = 0;
 
-    // Previous nodes and edges *to* current
-    for (let p of prev) {
-        const thisId = id++;
+    // Nodes
+    for (let n of nodes) {
         cy.add({
             data: {
-                id: thisId,
-                lbl: getNodeLbl(p, currLevel - 1),
-            }
-        })
-        cy.add({
-            data: {
-                id: id++,
-                source: thisId,
-                target: currId,
+                id: n.id,
+                lbl: getNodeLbl(n),
             }
         })
     }
 
-    // Next nodes and edges *from* current
-    for (let n of fwd) {
-        const thisId = id++;
+    // Edges
+    for (let e of edges) {
         cy.add({
-            data: {
-                id: thisId,
-                lbl: getNodeLbl(n, currLevel + 1),
-            }
-        })
-        cy.add({
-            data: {
-                id: id++,
-                source: currId,
-                target: thisId
-            }
+            data: e
         })
     }
 
